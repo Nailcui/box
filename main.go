81abb07cee2de2111ea072143e07f32e3576d4fb
nailcui <nailcui@foxmail.com>
2021-07-09 06:26:11 +0800

    feature: all ns

--- main.go
+++ main.go
@@ -12,8 +12,23 @@ func main() {
 	fmt.Println("box starting...")
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1,
+				HostID:      syscall.Getuid(),
+				Size:        1100,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1,
+				HostID:      syscall.Getgid(),
+				Size:        1100,
+			},
+		},
 	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
